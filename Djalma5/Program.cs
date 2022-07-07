97d20cf944c03fc2f4517736cd1fc9a26aed32ca
User <User@DESKTOP-CU12B7G>
2022-07-06 21:36:03 -0300

    Adicionar arquivos de projeto.

---
+++ Djalma5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Djalma5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("contador de pares");
+            for (int i = 2; i <= 100; i = i + 2) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
